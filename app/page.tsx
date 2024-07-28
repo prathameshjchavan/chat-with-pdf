6cf3982fbb7868a30886ec41d4bd77ec12de8806
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-28 18:02:16 +0530

    add homepage with banner section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,88 @@
+import {
+  BrainCogIcon,
+  EyeIcon,
+  GlobeIcon,
+  MonitorSmartphoneIcon,
+  ServerCogIcon,
+  ZapIcon,
+} from "lucide-react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+const FEATURES = [
+  {
+    name: "Store your PDF Documents",
+    description:
+      "Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon: GlobeIcon,
+  },
+  {
+    name: "Blazing Fast Responses",
+    description:
+      "Experience lightning-fast answers to your queries, ensuring you get the information you need instantly.",
+    icon: ZapIcon,
+  },
+  {
+    name: "Chat Memorisation",
+    description:
+      "Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
+    icon: BrainCogIcon,
+  },
+  {
+    name: "Interactive PDF Viewer",
+    description:
+      "Engage with your PDFs like never before using our intuitive and interactive viewer.",
+    icon: EyeIcon,
+  },
+  {
+    name: "Cloud Backup",
+    description:
+      "Rest assured knowing your documents are safely backed up on the cloud, protected from loss or damage.",
+    icon: ServerCogIcon,
+  },
+  {
+    name: "Responsive Across Devices",
+    description:
+      "Access and chat with your PDFs seamlessly on any device, whether it's your desktop, tablet, or smartphone.",
+    icon: MonitorSmartphoneIcon,
+  },
+];
+
 export default function Home() {
-	return (
-		<main className="">
-			<h1>Let&apos;s build a SaaS AI application!</h1>
-		</main>
-	);
+  return (
+    <main className="flex-1 overflow-y-auto bg-gradient-to-bl from-white to-indigo-600 p-2 lg:p-5">
+      <div className="rounded-md bg-white py-24 drop-shadow-xl sm:py-32">
+        <section className="mx-auto flex max-w-7xl flex-col items-center justify-center px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="text-base font-semibold leading-7 text-indigo-600">
+              Your interactive Document Companion
+            </h2>
+
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Transform Your PDFs into Interactive Conversations
+            </p>
+
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Introducing{" "}
+              <span className="font-bold text-indigo-600">Chat with PDF.</span>
+              <br />
+              <br /> Upload your document, and our chatbot will answer
+              questions, summarize content, and answer all your Qs. Ideal for
+              everyone, <span className="text-indigo-600">
+                Chat with PDF
+              </span>{" "}
+              turns static documents into{" "}
+              <span className="font-bold">dynamic conversations</span>,
+              enhancing productivity 10x fold effortlessly.
+            </p>
+          </div>
+
+          <Button asChild className="mt-10">
+            <Link href="/dashboard">Get Started</Link>
+          </Button>
+        </section>
+      </div>
+    </main>
+  );
 }
