faf3fad64e5691cc623c147caac01b60fdfc3181
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-29 12:57:34 +0530

    add features section

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
   ZapIcon,
 } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 
@@ -81,6 +82,38 @@ export default function Home() {
           <Button asChild className="mt-10">
             <Link href="/dashboard">Get Started</Link>
           </Button>
+        </section>
+
+        <section className="relative overflow-hidden pt-16">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <Image
+              src="https://i.imgur.com/VciRSTI.jpeg"
+              width={2432}
+              height={1442}
+              className="rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+              alt="app screenshot"
+            />
+            <div aria-hidden="true" className="relative">
+              <div className="absolute -inset-x-32 bottom-0 bg-gradient-to-t from-white/95 pt-[5%]" />
+            </div>
+          </div>
+        </section>
+
+        <section className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+            {FEATURES.map((feature) => (
+              <div className="relative pl-9" key={feature.name}>
+                <dt className="inline font-semibold text-gray-900">
+                  <feature.icon
+                    aria-hidden="true"
+                    className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                  />
+                </dt>
+
+                <dd>{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
         </section>
       </div>
     </main>
