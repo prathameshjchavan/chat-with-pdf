b2fc0901f1465fab496229d3912b706f6aad0235
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-30 15:55:02 +0530

    style dropzone

--- components/file-uploader.tsx
+++ components/file-uploader.tsx
@@ -1,7 +1,14 @@
 "use client";
 
-import { useCallback } from "react";
+import { Fragment, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import {
+  CheckCircleIcon,
+  CircleArrowDown,
+  HammerIcon,
+  RocketIcon,
+  SaveIcon,
+} from "lucide-react";
 
 const FileUploader = () => {
   const onDrop = useCallback((acceptedFiles: File[]) => {
@@ -19,13 +26,22 @@ const FileUploader = () => {
         className={`mt-10 flex h-96 w-[90%] items-center justify-center rounded-lg border-2 border-dashed border-indigo-600 p-10 text-indigo-600 ${isDragActive || isDragAccept ? "bg-indigo-300" : "bg-indigo-100"}`}
       >
         <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>
-            Drag &apos;n&apos; drop some files here, or click to select files
-          </p>
-        )}
+        <div className="flex flex-col items-center justify-center">
+          {isDragActive ? (
+            <Fragment>
+              <RocketIcon className="mb-5 h-20 w-20 animate-ping" />
+              <p>Drop the files here ...</p>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <CircleArrowDown className="h-20 w-20 animate-bounce" />
+              <p>
+                Drag &apos;n&apos; drop some files here, or click to select
+                files
+              </p>
+            </Fragment>
+          )}
+        </div>
       </div>
     </div>
   );
