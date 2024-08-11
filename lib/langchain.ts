8f5a88b6a60a39e431435cd1e212d8bedaccfad5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-08-11 18:50:13 +0530

    split the uploaded document

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -1,7 +1,18 @@
 import { auth } from "@clerk/nextjs/server";
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
-import pineconeClient from "./pinecone";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
+import { HumanMessage, AIMessage } from "@langchain/core/messages";
+import { PineconeStore } from "@langchain/pinecone";
+import { PineconeConflictError } from "@pinecone-database/pinecone/dist/errors";
 import { Index, RecordMetadata } from "@pinecone-database/pinecone";
+
+import { adminDb } from "@/firebaseAdmin";
+import pineconeClient from "./pinecone";
 
 const model = new ChatOpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -20,6 +31,44 @@ async function namespaceExists(
   return namespaces?.[namespace] !== undefined;
 }
 
+export async function generateDocs(docId: string) {
+  const { userId } = await auth();
+
+  if (!userId) throw new Error("User not found");
+
+  console.log("--- Fetching the download URL from Firebase... ---");
+
+  const firebaseRef = await adminDb
+    .collection("users")
+    .doc(userId)
+    .collection("files")
+    .doc(docId)
+    .get();
+  const downloadUrl = firebaseRef.data()?.downloadUrl;
+
+  console.log(`--- Download URL fetched successfully: ${downloadUrl} ---`);
+
+  // Fetch the PDF from the specified URL
+  const response = await fetch(downloadUrl);
+
+  // Load the PDF into a PDFDocument object
+  const data = await response.blob();
+
+  // Load the PDF document from the specified path
+  console.log("--- Loading PDF document... ---");
+  const loader = new PDFLoader(data);
+  const docs = await loader.load();
+
+  // Split the loaded document into smaller parts for easier processing
+  console.log("--- Splitting the document into smaller parts... ---");
+  const splitter = new RecursiveCharacterTextSplitter();
+  
+  const splitDocs = await splitter.splitDocuments(docs);
+  console.log(`--- Split into ${splitDocs.length} parts ---`);
+
+  return splitDocs;
+}
+
 export async function generateEmbeddingsInPineconeVectorStore(docId: string) {
   const { userId } = await auth();
 
@@ -31,4 +80,20 @@ export async function generateEmbeddingsInPineconeVectorStore(docId: string) {
   const embeddings = new OpenAIEmbeddings();
   const index = await pineconeClient.index(indexName);
   const namespaceAlreadyExists = await namespaceExists(index, docId);
+
+  if (namespaceAlreadyExists) {
+    console.log(
+      `--- Namespace ${docId} already exists, reusing existing embeddings... ---`,
+    );
+
+    pineconeVectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+      pineconeIndex: index,
+      namespace: docId,
+    });
+
+    return pineconeVectorStore;
+  } else {
+    // If the namespace does not exist, download the PDF from firestore via the stored Download URL & generate the embeddings and store them in the Pinecone vector store
+    const splitDocs = await generateDocs(docId);
+  }
 }
